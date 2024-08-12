fb5de7622854991fe2924e85f2ef6310819644b2
JENkt4k <james.e.nelson@gmail.com>
2024-08-12 16:35:20 -0500

    fixing global score

--- Global.gd
+++ Global.gd
@@ -5,7 +5,7 @@ var scores: SaveData
 var player_count: int = 1
 
 func _ready():
-	scores = SaveData.load_or_create(SaveData.data_path)
+	scores = SaveData.load_or_create("user://testdata.tres")#SaveData.data_path)
 
 #var score1 = 0
 #var score2 = 0
--- data/SaveData.gd
+++ data/SaveData.gd
@@ -1,16 +1,17 @@
 class_name SaveData extends Resource
 @export var scores: Dictionary = {} # "name,score,time":<score int>
 #@export var data_path = "user://testdata.tres"
-static var data_path = "user://testdata.tres"
+var data_path = "user://testdata.tres"
 
 func save()->void:
 	ResourceSaver.save(self, data_path)
 	
 static func load_or_create(_data_path) -> SaveData:
-	data_path = _data_path
-	var res: SaveData = load(data_path) as SaveData
+	#data_path = _data_path
+	var res: SaveData = load(_data_path) as SaveData
 	if !res:
 		res = SaveData.new()
+		res.data_path = _data_path
 	return res
 	
 	
